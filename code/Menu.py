1aa2491cf25f9bb673b74ae482e4f482e81e75b9
marcio@marcio.com <marcioalvinho@gmail.com>
2024-11-02 22:47:51 -0300

    Creating asset folder Inserting all assets Creating the const file

--- code/Menu.py
+++ code/Menu.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+
+from msilib.schema import Font
+
 import pygame
+from pygame import Surface, Rect
+
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION,  COLOR_WHITE
 
 
 class Menu:
@@ -15,6 +21,12 @@ class Menu:
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Mountain",COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 *i))
+
             pygame.display.flip()
 
             # Check for all events
@@ -22,3 +34,9 @@ class Menu:
                 if event.type == pygame.QUIT:
                    pygame.quit()  # Close Window
                    quit()  # end pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple) -> object:
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
