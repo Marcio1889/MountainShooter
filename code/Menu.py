a601fb9f69a308930e3d1925711d1af4a9822e7c
marcio@marcio.com <marcioalvinho@gmail.com>
2024-11-09 15:47:05 -0300

    Scrolling the Menu

--- code/Menu.py
+++ code/Menu.py
@@ -31,13 +31,25 @@ class Menu:
                 else:
                     self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 *i))
 
-            pygame.display.flip()
-
             # Check for all events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                    pygame.quit()  # Close Window
                    quit()  # end pygame
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+
+            pygame.display.flip()
+
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple) -> object:
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
