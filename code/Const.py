b56c2899cc3f28c64285097d51b2268f9b1a0fe8
marcio@marcio.com <marcioalvinho@gmail.com>
2024-11-24 23:51:49 -0300

    Level 2 implementation

--- code/Const.py
+++ code/Const.py
@@ -11,14 +11,22 @@ COLOR_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 1
 ENTITY_SPEED = {
-    'level1Bg0': 0,
-    'level1Bg1': 1,
-    'level1Bg2': 2,
-    'level1Bg3': 3,
-    'level1Bg4': 4,
-    'level1Bg5': 5,
-    'level1Bg6': 6,
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
+    'Level2Bg5': 5,
+    'Level2Bg6': 6,
     'Player1' : 3,
     'Player1Shot': 3,
     'Player2' : 3,
@@ -30,13 +38,20 @@ ENTITY_SPEED = {
 }
 
 ENTITY_HEALTH = {
-    'level1Bg0': 999,
-    'level1Bg1':999,
-    'level1Bg2':999,
-    'level1Bg3':999,
-    'level1Bg4':999,
-    'level1Bg5':999,
-    'level1Bg6':999,
+    'Level1Bg0': 999,
+    'Level1Bg1':999,
+    'Level1Bg2':999,
+    'Level1Bg3':999,
+    'Level1Bg4':999,
+    'Level1Bg5':999,
+    'Level1Bg6':999,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
+    'Level2Bg5': 999,
+    'Level2Bg6': 999,
     'Player1':300,
     'Player1Shot': 1,
     'Player2':300,
@@ -48,17 +63,20 @@ ENTITY_HEALTH = {
 }
 
 ENTITY_DAMAGE = {
-    'level1Bg0': 0,
-    'level1Bg1': 0,
-    'level1Bg2': 0,
-    'level1Bg3': 0,
-    'level1Bg4': 0,
-    'level1Bg5': 0,
-    'level1Bg6': 0,
-    'level2Bg1': 0,
-    'level2Bg2': 0,
-    'level2Bg3': 0,
-    'level2Bg4': 0,
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Level2Bg5': 0,
+    'Level2Bg6': 0,
     'Player1': 1,
     'Player1Shot': 25,
     'Player2': 1,
@@ -71,17 +89,18 @@ ENTITY_DAMAGE = {
 }
 
 ENTITY_SCORE = {
-    'level1Bg0': 0,
-    'level1Bg1': 0,
-    'level1Bg2': 0,
-    'level1Bg3': 0,
-    'level1Bg4': 0,
-    'level1Bg5': 0,
-    'level1Bg6': 0,
-    'level2Bg1': 0,
-    'level2Bg2': 0,
-    'level2Bg3': 0,
-    'level2Bg4': 0,
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 0,
     'Player1Shot': 0,
     'Player2': 0,
@@ -98,9 +117,6 @@ ENTITY_SHOT_DELAY = {
     'Enemy1': 100,
     'Enemy2': 200,
 }
-# S
-SPAWN_TIME = 4000
-
 
 #M
 MENU_OPTION = ('NEW GAME 1P',
@@ -120,6 +136,14 @@ PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
                     'Player2': pygame.K_d}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                     'Player2': pygame.K_LCTRL}
+
+# S
+SPAWN_TIME = 4000
+
+#T
+TIMEOUT_STEP = 100
+TIMEOUT_LEVEL = 20000
+
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
--- code/Game.py
+++ code/Game.py
@@ -4,7 +4,7 @@ import pygame
 
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.Menu import Menu
-from code.level import level  # Certifique-se de que essa classe existe no arquivo correspondente.
+from code.Level import  Level  # Certifique-se de que essa classe existe no arquivo correspondente.
 
 class Game:
     def __init__(self):
@@ -17,10 +17,14 @@ class Game:
             menu_return = menu.run()
 
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                player_score = [0,0] # [Player1, Player2]
                 # Cria uma instância da classe Level
-                current_level = level(self.window, 'Level1', menu_return)
-                level_return = current_level.run()
-                # Você pode decidir o que fazer com level_return, se necessário.
+                level = Level(self.window, 'Level1', menu_return, player_score)
+                level_return = level.run(player_score)
+                if level_return:
+                    level = Level(self.window, 'Level2', level_return)
+                    level_return = level.run()
+
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
                 quit()
